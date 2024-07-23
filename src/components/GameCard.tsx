c43914f3c82bca2b12c3929bed86b0a7f33c32d7
Abdullah Ashraf <abdullah.ashraf4143@outlook.com>
2024-07-23 23:23:19 +0500

    Removed the Image optimiazation

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -2,7 +2,6 @@ import { Card, CardBody, HStack, Img, Text } from "@chakra-ui/react";
 import { Game } from "../hooks/useGame"
 import PlatformIconList from "./PlatformIconList";
 import CriticScore from "./CriticScore";
-import GetCroppedImageUrl from "../services/GetCroppedImageUrl";
 
 interface Props{
   game : Game;
@@ -10,7 +9,7 @@ interface Props{
 const GameCard = ({game} : Props) => {
   return (
     <Card>
-      <Img src={GetCroppedImageUrl(game.background_image)}/>
+      <Img src={game.background_image}/>
       <CardBody>
         <Text fontSize='lg' fontWeight={700}>{game.name}</Text>
         <HStack justifyContent={'space-between'}>
